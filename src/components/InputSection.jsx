6e30775d69be8a093c25be56fbc58149bf7df685
JustNormalProgrammer <testkonto1290@gmail.com>
2024-12-13 18:37:57 +0100

    Add work experience section

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -33,13 +33,25 @@ function EducationalSection() {
   return (
     <div className="input-subsection">
       <form action="">
-        <label htmlFor="school-name">School name:
-          <input type="text" id="school-name" name="school-name" placeholder="University of Wonderland" />
+        <label htmlFor="school-name">
+          School name:
+          <input
+            type="text"
+            id="school-name"
+            name="school-name"
+            placeholder="University of Wonderland"
+          />
         </label>
         <label htmlFor="title-of-study">
           College Degree:
-          <select name="title-of-study" id="title-of-study" style={{width: "100%", padding: "3px"}}>
-            <option value="associate" selected>Associate</option>
+          <select
+            name="title-of-study"
+            id="title-of-study"
+            style={{ width: "100%", padding: "3px" }}
+          >
+            <option value="associate" selected>
+              Associate
+            </option>
             <option value="bachelors">Bachelor&apos;s</option>
             <option value="associate">Master&apos;s</option>
             <option value="associate">Doctoral</option>
@@ -57,11 +69,33 @@ function EducationalSection() {
     </div>
   );
 }
+function PracticalExperienceSection() {
+  return (
+    <div className="input-subsection">
+      <form action="">
+        <label htmlFor="company-name"> 
+            Company name: 
+          <input
+            type="text"
+            name="company-name"
+            id="company-name"
+            placeholder="Odin Company"
+          />
+        </label>
+        <div className="button-group">
+          <button type="reset">Reset</button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
 export default function InputSection() {
   return (
     <div className="input-container">
       <GeneralSection />
-      <EducationalSection/>
+      <EducationalSection />
+      <PracticalExperienceSection/>
     </div>
   );
 }
